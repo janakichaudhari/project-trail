ed77492b367afe97db57b1ac848c640622434ecf
janakichaudhari <68949515+janakichaudhari@users.noreply.github.com>
2020-09-17 14:12:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-trail
+# SupplyMission
---
+++ sketch.js
@@ -0,0 +1,84 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Bodies;
+var rec1,rec2,rec3;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	rec1=createSprite(width-340,height-60,20,60);
+	fill("red");
+	rect(0, 0, this.width, this.height);
+	pop();
+
+	rec2=createSprite(width-460,height-60,20,60);
+	fill("red");
+	rect(0, 0, this.width, this.height);
+	pop();
+	
+	rec3=createSprite(width/2,height-40,120,20);
+    fill("red"); 
+	rect(0, 0, this.width, this.height);
+	pop();
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	var opt={
+        isStatic :false
+
+	}
+		
+}
+}
+
+
+
